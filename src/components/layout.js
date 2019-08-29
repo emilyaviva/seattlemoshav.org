8135a4ac26c513ba6017f4c6a9eaa06ad0b3d05a
Emily Kapor-Mater <emily@emilyaviva.com>
2019-08-28 19:15:32 -0700

    Remove gatsby-background-image use

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,44 +7,52 @@ import SiteHeader from "./site-header"
 import SiteFooter from "./site-footer"
 import "./layout.css"
 
-const Layout = ({ className, children }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        desktop: file(relativePath: { eq: "seattle-unsplash.jpg" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      const imageData = data.desktop.childImageSharp.fluid
-      return (
-        <BackgroundImage
-          Tag="div"
-          className={className}
-          fluid={imageData}
-          backgroundColor={`#040e18`}
-        >
-          <SiteHeader />
-          <main>{children}</main>
-          <SiteFooter />
-        </BackgroundImage>
-      )
-    }}
-  />
+// const Layout = ({ className, children }) => (
+//   <StaticQuery
+//     query={graphql`
+//       query {
+//         desktop: file(relativePath: { eq: "seattle-unsplash.jpg" }) {
+//           childImageSharp {
+//             fluid(quality: 90, maxWidth: 1920) {
+//               ...GatsbyImageSharpFluid_withWebp
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={data => {
+//       const imageData = data.desktop.childImageSharp.fluid
+//       return (
+//         <BackgroundImage
+//           Tag="div"
+//           className={className}
+//           fluid={imageData}
+//           backgroundColor={`#040e18`}
+//         >
+//           <SiteHeader />
+//           <main>{children}</main>
+//           <SiteFooter />
+//         </BackgroundImage>
+//       )
+//     }}
+//   />
+// )
+
+// Layout.propTypes = {
+//   className: PropTypes.string,
+//   children: PropTypes.node.isRequired
+// }
+
+// Layout.defaultProps = {
+//   className: `BackgroundImage`
+// }
+
+const Layout = ({ children }) => (
+  <>
+    <SiteHeader />
+    <main>{children}</main>
+    <SiteFooter />
+  </>
 )
-
-Layout.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired
-}
-
-Layout.defaultProps = {
-  className: `BackgroundImage`
-}
 
 export default Layout
