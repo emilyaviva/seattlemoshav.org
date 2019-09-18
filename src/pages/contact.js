5be2e38ad68a7c41107130508aebecab9214b3b6
Emily Kapor-Mater <emily@emilyaviva.com>
2019-09-17 21:15:56 -0700

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,9 +7,52 @@ import SEO from "../components/seo"
 export default () => (
   <Layout>
     <SEO title="Contact Us" />
-    <h1>Contact Us</h1>
-    <p>Link to email, and so forth</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="is-hidden-mobile" style={{ padding: `2rem 0` }} />
+    <h1 className="title is-1 has-text-centered">Contact Us</h1>
+    <div className="columns">
+      <form
+        className="column is-half is-offset-one-quarter"
+        action="/contact-form"
+        method="post"
+        id="contact-form"
+        style={{ background: `cornsilk`, borderRadius: `3px`, border: `2px solid black` }}
+      >
+        <div className="field">
+          <label className="label">Name</label>
+          <div className="control">
+            <input required className="input" type="text" placeholder="Your name" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control">
+            <input required className="input" type="email" placeholder="Your email address" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Message</label>
+          <div className="control">
+            <textarea class="textarea" placeholder="Optional message"></textarea>
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <label class="checkbox">
+              <input type="checkbox" />
+              {' '}Subscribe to our newsletter
+            </label>
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link">Submit</button>
+          </div>
+          <div className="control">
+            <Link className="button is-link" to="/">Cancel</Link>
+          </div>
+        </div>
+      </form>
+    </div>
   </Layout>
 )
 
